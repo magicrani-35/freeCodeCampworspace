568445f51207dae848d1be1617af375d0b2bccdd
magicrani-35 <babs2818@icloud.com>
2025-08-31 11:53:59 -0400

    Create lab1.js

---
+++ JavaScript/lab1.js
@@ -0,0 +1,12 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Dale";
+let botLocation = "Utopia";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Did you know? JavaScript was created in just 10 days!";
+console.log(codingFact);
