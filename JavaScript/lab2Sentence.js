2d4ffe87e6789792594b641e96450428355d6ed3
magicrani-35 <babs2818@icloud.com>
2025-09-16 14:39:32 -0400

    Update lab2Sentence.js

--- JavaScript/lab2Sentence.js
+++ JavaScript/lab2Sentence.js
@@ -1,11 +1,23 @@
-let adjective = "exciting";
-let noun = "adventure";
-let verb = "explore";
-let place = "mountains";
-let adjective2 = "breathtaking";
-let noun2 = "views";
+let adjective = "Large";
+let noun = "Dragon";
+let verb = "thinking";
+let place = "cottage in the mountain ";
+let adjective2 = "flared";
+let noun2 = "strawberries";
 
-let firstStory = "Once upon a time there was a " + noun + " that has such " + adjective + noun + "who loved to eat"
-place + ". It was known for its " + adjective2 + " " + noun2 + ".";
 
-//*You should use the following story template to create the first story and assign it to the firstStory variable: "Once upon a time, there was a(n) [adjective] [noun] who loved to eat [noun2]. The [noun] lived in a [place] and had [adjective2] nostrils that blew fire when it was [verb]."; *//
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: " + firstStory);
+
+adjective = "Hyper";
+noun = "cheetah";
+verb = "running";
+place = "beach house";
+adjective2 = "spotted";
+noun2 = "Gazelles";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+
+console.log("Second story: " + secondStory);
